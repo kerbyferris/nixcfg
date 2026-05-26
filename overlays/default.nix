cd352d9eb518bab6822f8b6a2584b71d7d75f247
Kerby Ferris <kerbyferris@gmail.com>
2026-05-27 00:20:26 +0100

    bitwig-connect-control-panel: fix Exec symlink, use impure path for vendor .deb
    - Add bitwig-control-panel -> bitwig-connect-control-panel symlink
    - src passed via overlay from vendor/ (gitignored, requires --impure)

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,7 +3,9 @@
   additions = final: prev: {
     vidplayvst = final.callPackage ../pkgs/vidplayvst.nix {};
     bitwig-fhs = final.callPackage ../pkgs/bitwig-fhs.nix {};
-    bitwig-connect-control-panel = final.callPackage ../pkgs/bitwig-connect-control-panel.nix {};
+    bitwig-connect-control-panel = final.callPackage ../pkgs/bitwig-connect-control-panel.nix {
+      src = ../vendor/bitwig-connect-control-panel-1.0.deb;
+    };
   };
 
   # Your other overlays below are fine and should remain
--- pkgs/bitwig-connect-control-panel.nix
+++ pkgs/bitwig-connect-control-panel.nix
@@ -33,104 +33,108 @@
   xcbutilwm,
   xcb-imdkit,
   zlib,
+  src ? null,
 }:
-stdenv.mkDerivation (finalAttrs: {
-  pname = "bitwig-connect-control-panel";
-  version = "1.0";
+assert src != null;
+  stdenv.mkDerivation (finalAttrs: {
+    pname = "bitwig-connect-control-panel";
+    version = "1.0";
 
-  src = ../vendor/bitwig-connect-control-panel-1.0.deb;
+    inherit src;
 
-  strictDeps = true;
+    strictDeps = true;
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-    dpkg
-    makeBinaryWrapper
-    wrapGAppsHook3
-  ];
+    nativeBuildInputs = [
+      autoPatchelfHook
+      dpkg
+      makeBinaryWrapper
+      wrapGAppsHook3
+    ];
 
-  buildInputs = [
-    alsa-lib
-    atk
-    cairo
-    fontconfig
-    freetype
-    gdk-pixbuf
-    giflib
-    glib
-    gtk3
-    harfbuzz
-    lcms2
-    libglvnd
-    libjack2
-    libjpeg8
-    libusb1
-    libx11
-    libxcb
-    libxcursor
-    libxfixes
-    libxkbcommon
-    libxrender
-    libxtst
-    pango
-    vulkan-loader
-    xcb-imdkit
-    xcbutilwm
-    zlib
-    (lib.getLib stdenv.cc.cc)
-    systemd
-  ];
+    buildInputs = [
+      alsa-lib
+      atk
+      cairo
+      fontconfig
+      freetype
+      gdk-pixbuf
+      giflib
+      glib
+      gtk3
+      harfbuzz
+      lcms2
+      libglvnd
+      libjack2
+      libjpeg8
+      libusb1
+      libx11
+      libxcb
+      libxcursor
+      libxfixes
+      libxkbcommon
+      libxrender
+      libxtst
+      pango
+      vulkan-loader
+      xcb-imdkit
+      xcbutilwm
+      zlib
+      (lib.getLib stdenv.cc.cc)
+      systemd
+    ];
 
-  dontWrapGApps = true;
+    dontWrapGApps = true;
 
-  unpackPhase = ''
-    runHook preUnpack
-    dpkg-deb -x $src .
-    runHook postUnpack
-  '';
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/libexec
-    cp -r opt/bitwig-control-panel/* $out/libexec/
-
-    mkdir -p $out/share
-    cp -r usr/share/applications $out/share/
-    cp -r usr/share/icons $out/share/
-    cp -r usr/share/mime $out/share/
-
-    runHook postInstall
-  '';
-
-  postFixup = let
-    wrapperScript = ''
-      #!${stdenv.shell}
-      exec "''${BASH_SOURCE[0]%/*}"/../libexec/BitwigConnectControlPanel "$@"
+    unpackPhase = ''
+      runHook preUnpack
+      dpkg-deb -x $src .
+      runHook postUnpack
     '';
-  in ''
-    wrapProgram "$out/libexec/bin/show-file-dialog-gtk3" \
-      "''${gappsWrapperArgs[@]}"
 
-    install -Dm755 /dev/stdin "$out/bin/bitwig-connect-control-panel" <<'EOF'
-    ${wrapperScript}
-    EOF
+    installPhase = ''
+      runHook preInstall
 
-    mkdir -p $out/etc/udev/rules.d
-    cat > $out/etc/udev/rules.d/00-bitwig.rules <<'RULES'
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="17c8", ATTRS{idProduct}=="7010", MODE:="0666"
-    KERNEL=="hidraw*", ATTRS{idVendor}=="17c8", ATTRS{idProduct}=="7010", MODE:="0666"
+      mkdir -p $out/libexec
+      cp -r opt/bitwig-control-panel/* $out/libexec/
 
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="17c8", ATTRS{idProduct}=="7011", MODE:="0666"
-    RULES
-  '';
+      mkdir -p $out/share
+      cp -r usr/share/applications $out/share/
+      cp -r usr/share/icons $out/share/
+      cp -r usr/share/mime $out/share/
 
-  meta = {
-    description = "Control panel for Bitwig Connect audio interface";
-    homepage = "https://bitwig.com";
-    license = lib.licenses.unfree;
-    platforms = ["x86_64-linux"];
-    sourceProvenance = [lib.sourceTypes.binaryNativeCode];
-    mainProgram = "bitwig-connect-control-panel";
-  };
-})
+      runHook postInstall
+    '';
+
+    postFixup = let
+      wrapperScript = ''
+        #!${stdenv.shell}
+        exec "''${BASH_SOURCE[0]%/*}"/../libexec/BitwigConnectControlPanel "$@"
+      '';
+    in ''
+      wrapProgram "$out/libexec/bin/show-file-dialog-gtk3" \
+        "''${gappsWrapperArgs[@]}"
+
+      install -Dm755 /dev/stdin "$out/bin/bitwig-connect-control-panel" <<'EOF'
+      ${wrapperScript}
+      EOF
+
+      ln -s bitwig-connect-control-panel "$out/bin/bitwig-control-panel"
+
+      mkdir -p $out/etc/udev/rules.d
+      cat > $out/etc/udev/rules.d/00-bitwig.rules <<'RULES'
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="17c8", ATTRS{idProduct}=="7010", MODE:="0666"
+      KERNEL=="hidraw*", ATTRS{idVendor}=="17c8", ATTRS{idProduct}=="7010", MODE:="0666"
+
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="17c8", ATTRS{idProduct}=="7011", MODE:="0666"
+      RULES
+    '';
+
+    meta = {
+      description = "Control panel for Bitwig Connect audio interface";
+      homepage = "https://bitwig.com";
+      license = lib.licenses.unfree;
+      platforms = ["x86_64-linux"];
+      sourceProvenance = [lib.sourceTypes.binaryNativeCode];
+      mainProgram = "bitwig-connect-control-panel";
+    };
+  })
