b692f1f8550512354556b40f812c5ccb1d1bc082
Kerby Ferris <kerbyferris@gmail.com>
2026-05-26 23:26:35 +0100

    bitwig-fhs: add desktop entry so Rofi can launch via FHS wrapper
    Restore bitwig-studio6 to home packages for icons, add xdg.desktopEntries
    override that points Exec to bitwig-studio-fhs-env

--- home/features/audio/default.nix
+++ home/features/audio/default.nix
@@ -10,8 +10,40 @@ in {
   options.features.audio.enable = mkEnableOption "enable audio production config";
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
+      bitwig-studio6
       vcv-rack
     ];
+
+    xdg.desktopEntries."com.bitwig.BitwigStudio" = {
+      name = "Bitwig Studio";
+      genericName = "Digital Audio Workstation";
+      comment = "Modern music production and performance";
+      exec = "bitwig-studio-fhs-env";
+      icon = "com.bitwig.BitwigStudio";
+      terminal = false;
+      type = "Application";
+      categories = ["AudioVideo" "Music" "Audio" "Sequencer" "Midi" "Mixer" "Player" "Recorder"];
+      mimeType = [
+        "application/bitwig-clip"
+        "application/bitwig-device"
+        "application/bitwig-package"
+        "application/bitwig-preset"
+        "application/bitwig-project"
+        "application/bitwig-scene"
+        "application/bitwig-template"
+        "application/bitwig-extension"
+        "application/bitwig-remote-controls"
+        "application/bitwig-module"
+        "application/bitwig-modulator"
+        "application/vnd.bitwig.dawproject"
+      ];
+      settings = {
+        Keywords = "daw;bitwig;audio;midi";
+        StartupNotify = "true";
+        StartupWMClass = "com.bitwig.BitwigStudio";
+        DBusActivatable = "false";
+      };
+    };
 
     # Create the symlink for the main VST plugin
     home.file.".vst/VidPlayVSTv2.so" = {
